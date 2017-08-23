9b1173340bc19c9ba299837a23026070028745bd
lucypow <lucyna.powalowska@gmail.com>
2017-08-23 23:14:54 +0200

    feature/FZ5WK-76/ added forms for SignIn and SignUp components

--- src/components/authorization/SignUp.js
+++ src/components/authorization/SignUp.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import {Button} from 'react-bootstrap'
 
 const SignUp = () => (
 
     <div>
         <h1>Sign Up form</h1>
+        <form>
+            <fieldset className="form-group">
+                <label>Email:</label>
+                <input className="form-control"/>
+            </fieldset>
+            <fieldset className="form-group">
+                <label>Password:</label>
+                <input className="form-control" type="password"/>
+            </fieldset>
+            <fieldset className="form-group">
+                <label>Confirm Password:</label>
+                <input className="form-control" type="password"/>
+            </fieldset>
+            <Button action="submit" bsStyle="success">Sign up!</Button>
+        </form>
     </div>
 )
 
