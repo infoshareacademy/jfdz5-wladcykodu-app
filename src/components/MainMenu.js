67c1d3ca3f8314309784d987143db7d5b1478a77
Lucyna Powalowska <lucyna.powalowska@gmail.com>
2017-08-26 18:31:08 +0200

    feature/ warnings fixed

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,19 +1,16 @@
 import React from 'react'
-
 import {Nav, Navbar, NavItem} from 'react-bootstrap'
 import {LinkContainer} from 'react-router-bootstrap'
-
 const MainMenu = () => (
     <div>
-
-       <Navbar inverse>
+        <Navbar inverse>
             <Navbar.Header>
                 <Navbar.Brand>
-                    <a href="#">
-                        <img src={require('../images/logo-image.png')} />
+                    <a href="/">
+                        <img src={require('../images/logo-image.png')} alt="logo of auto parts app"/>
                     </a>
-                    <a href="#">
-                        <img src={require('../images/logo-name.png')} alt="" />
+                    <a href="/">
+                        <img src={require('../images/logo-name.png')} alt="logo of auto parts app"/>
                     </a>
                 </Navbar.Brand>
             </Navbar.Header>
@@ -21,27 +18,22 @@ const MainMenu = () => (
                 <LinkContainer exact to="/">
                     <NavItem>Home</NavItem>
                 </LinkContainer>
-                <img src={'C:\\Users\\ajemi\\WebstormProjects\\jfdz5-wladcykodu-app\\src\\images\\logo.png'} />
                 <LinkContainer to="/signin">
                     <NavItem>Sign In</NavItem>
                 </LinkContainer>
-
                 <LinkContainer to="/signup">
                     <NavItem>Sign Up</NavItem>
                 </LinkContainer>
                 <LinkContainer to="/userpanel">
                     <NavItem>User Panel</NavItem>
                 </LinkContainer>
-
-
             </Nav>
-           <Nav pullRight>
-               <LinkContainer to="#">
-                   <NavItem>Log Out</NavItem>
-               </LinkContainer>
-           </Nav>
+            <Nav pullRight>
+                <LinkContainer to="#">
+                    <NavItem>Log Out</NavItem>
+                </LinkContainer>
+            </Nav>
         </Navbar>
     </div>
 )
-
 export default MainMenu
