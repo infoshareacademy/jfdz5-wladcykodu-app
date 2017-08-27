0ab6177606d352c07e23da5b6a9322dee97fb54b
Lucyna Powalowska <lucyna.powalowska@gmail.com>
2017-08-27 18:40:12 +0200

    feature/FZ5WK-69/ changed HomeView

--- src/components/views/HomeView.js
+++ src/components/views/HomeView.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import {Grid, Col} from 'react-bootstrap'
 
 const HomeView = () => (
-    <div>
-        <h1>Home</h1>
-    </div>
+    <Grid>
+        <Col lg={6} lgPush={3} xs={12} sm={12} md={6} mdPush={3} style={{textAlign: "center"}}>
+            <h1>Welcome in our App :)</h1>
+            <h2>Home View...</h2>
+
+        </Col>
+    </Grid>
 )
 export default HomeView
