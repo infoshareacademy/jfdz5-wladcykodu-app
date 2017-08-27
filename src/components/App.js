5fa7aa00f8eefa95cdba3e1b79de307eb0c1aa0c
Ola Jemielity <ajemielity@o2.pl>
2017-08-27 20:54:24 +0200

    working version of footer

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 import MainMenu from './MainMenu'
 import Content from './Content'
+import Footer from './Footer'
 
 const App = () => (
     <div>
         <MainMenu/>
         <Content/>
-        {/*  <Sidebar/>
-         <Footer/>*/}
+        {/*  <Sidebar/>*/}
+         <Footer/>
     </div>
 )
 
---
+++ src/components/Footer.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const FooterContainer = styled.div`
+	bottom: 0;
+	background: red;
+	position: absolute;
+	left: 0;
+	right: 0;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+`
+
+
+
+
+const Footer = () => (
+    <FooterContainer>
+        <div>
+            <p>Władcy Kodu © 2017</p>
+            <img src={require('../images/facebook.svg')} alt=""/>
+            <img src={require('../images/twitter.svg')} alt=""/>
+            <img src={require('../images/github.svg')} alt=""/>
+        </div>
+        <div>
+            <h4>About us</h4>
+            <p>WWW</p>
+        </div>
+        <div>
+            <h4>For users</h4>
+            <p>FAQ</p>
+        </div>
+        <div>
+            <p>Władcy Kodu<br/>
+                Grunwaldzka Street<br/>
+                Gdansk, Poland<br/>
+                +48 777 77 77<br/>
+                support@wladcykodu.com</p>
+        </div>
+
+    </FooterContainer>
+)
+export default Footer
