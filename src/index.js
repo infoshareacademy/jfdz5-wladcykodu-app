36036f080471bfe11def25d40862b47b1083e9c0
Lucyna Powalowska <lucyna.powalowska@gmail.com>
2017-08-27 18:13:35 +0200

    feature/FZ5WK-69/ added redux and store.js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,24 @@ import ReactDOM from 'react-dom'
 import {
     BrowserRouter as Router
 } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
-import './index.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/css/bootstrap-theme.css'
+
+import './index.css'
+
+import store from './store'
 
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
-    <Router>
-        <App/>
-    </Router>,
+    <Provider store={store}>
+        <Router>
+            <App/>
+        </Router>
+    </Provider>,
     document.getElementById('root')
 )
 
