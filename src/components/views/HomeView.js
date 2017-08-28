78d38c52f3a9bd02be9148f7e59a4f0a42062559
lucypow <lucyna.powalowska@gmail.com>
2017-08-28 21:51:33 +0200

    feature/FZ5WK-76/ fetched data from api and displayed car brand names

--- src/components/views/HomeView.js
+++ src/components/views/HomeView.js
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, {Component} from 'react';
 import {Grid, Col} from 'react-bootstrap'
 
-const HomeView = () => (
-    <Grid>
-        <Col lg={6} lgPush={3} xs={12} sm={12} md={6} mdPush={3} style={{textAlign: "center"}}>
-            <h1>Welcome in our App :)</h1>
-            <h2>Home View...</h2>
+class HomeView extends Component {
 
-        </Col>
-    </Grid>
-)
+    constructor(props, context) {
+        super(props, context)
+        this.state = {items: []}
+    }
+
+    componentDidMount() {
+        fetch('/api/v2?lang=polish')
+            .then(result => result.json())
+            .then(res => {
+                const items = res.data
+                this.setState({items})
+            })
+    }
+
+    render() {
+
+        return (
+
+            <div>
+                <Grid>
+                    <Col lg={6} lgPush={3} xs={12} sm={12} md={6} mdPush={3} style={{textAlign: "center"}}>
+                        <h1>Welcome in our App :)</h1>
+                        <h2>Home View...</h2>
+                        <ul>
+                            {this.state.items.length ?
+                                this.state.items.map(item => <li key={item.id}>{item.name}</li>)
+                                : <li>Loading...</li>
+                            }
+                        </ul>
+                    </Col>
+                </Grid>
+            </div>
+        );
+    }
+}
+
 export default HomeView
