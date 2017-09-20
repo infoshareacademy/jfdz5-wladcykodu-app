6dfdc2567fefa63a4253e94b97712b6d3bed9293
lucypow <lucyna.powalowska@gmail.com>
2017-09-20 23:13:02 +0200

    now application is available only for signed in/up users

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,68 @@
-import React from 'react'
-
+import React, {Component} from 'react'
+import {firebaseApp} from '../firebase'
+import {Grid, Col, Image, Tabs, Tab} from 'react-bootstrap'
 import MainMenu from './MainMenu'
 import Content from './Content'
 import Footer from './Footer'
+import SignIn from './authorization/SignIn'
+import SignUp from './authorization/SignUp'
+import './App.css'
 
-const App = () => (
-    <div>
-        <MainMenu/>
-        <Content/>
-        {/*  <Sidebar/>*/}
-         <Footer/>
-    </div>
-)
+
+class App extends Component {
+
+    state = {
+        user: null,
+    }
+
+    componentDidMount() {
+        firebaseApp.auth().onAuthStateChanged(user => {
+            if (user) {
+                this.setState({
+                    user: user
+                })
+            } else {
+                this.setState({
+                    user: null
+                })
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className="app-container">
+                {
+                    this.state.user === null ?
+                        (<div className="sign-container">
+                                <Grid>
+                                    <Col xs={6} className="sign-in-up">
+                                        <Image src={require('../images/logo-image.png')} alt="logo of auto parts app"/>
+                                        <h2>Welcome... to search from our awesome car parts, please sign in/up</h2>
+
+                                        <Tabs defaultActiveKey={1} id="tab">
+                                            <Tab eventKey={1} title="Sign in">
+                                                <SignIn/>
+                                            </Tab>
+
+                                            <Tab eventKey={2} title="Sign up">
+                                                <SignUp/>
+                                            </Tab>
+                                        </Tabs>
+                                    </Col>
+                                </Grid>
+                            </div>
+                        )
+                        :
+                        <div>
+                            <MainMenu/>
+                            <Content/>
+                            <Footer/>
+                        </div>
+                }
+            </div>
+        )
+    }
+}
 
 export default App
-
-
