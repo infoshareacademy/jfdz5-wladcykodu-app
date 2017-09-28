ac3e6c865b5a8bf26bdb4751be113a8219f69fff
lucypow <lucyna.powalowska@gmail.com>
2017-09-28 22:03:22 +0200

    added tabs and grid view

--- src/components/views/PartsList.js
+++ src/components/views/PartsList.js
@@ -1,17 +1,21 @@
 import React, {Component} from 'react'
-import {ListGroup, Grid, Col, Row, Button, Panel, Image} from 'react-bootstrap'
+import {ListGroup, Grid, Col, Row, Button, Panel, Image, Tab, Tabs, Thumbnail} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
 import * as firebase from 'firebase'
 import FaStar from 'react-icons/lib/fa/star'
 import './partslist.css'
 import {API_URL} from '../App'
 import {connect} from 'react-redux'
+import '../App.css'
 
 class PartsList extends Component {
   state = {
     parts: [],
-    favorites: []
+    favorites: [],
+    tabKey: 1
   }
+
+  handleTabChange = key => this.setState({tabKey: key})
 
   handleAddToFav = (item) => {
     const user = firebase.auth().currentUser
@@ -60,53 +64,93 @@ class PartsList extends Component {
       <div>
         <Grid>
           <Col style={{textAlign: "center"}}>
-            <h2>Select part:</h2>
-            <ListGroup>
-              {
-                this.state.parts.length ?
-                  this.state.parts.map(
-                    (item, m) => {
-                      return (
-                        <div key={m}>
-                          <Panel>
-                            <Grid>
-                              <Row>
-                                <Col xs={6} md={4}>
-                                  <Image responsive src={item.part.jpg[0]}
-                                         alt="Picture of part"
-                                         className="img-part"/>
-                                  <h3>{item.part.data.name}</h3>
+            <div>
+              <Tabs activeKey={this.state.tabKey} id="tab" onSelect={this.handleTabChange}>
+                <Tab eventKey={1} title="List">
+                  <ListGroup>
+                    {
+                      this.state.parts.length ?
+                        this.state.parts.map(
+                          (item, m) => {
+                            return (
+                              <div key={m}>
+                                <Panel>
+                                  <Grid>
+                                    <Row>
+                                      <Col xs={6} md={4}>
+                                        <Image responsive src={item.part.jpg[0]}
+                                               alt="Picture of part"
+                                               className="img-part"/>
+                                        <h3>{item.part.data.name}</h3>
+                                      </Col>
+                                      <Col xs={6} md={8}>
+                                        <p>Brand: <span className="text-info">{item.part.data.brand}</span>
+                                        </p>
+                                        <p>Number: <span className="text-info">{item.part.data.number}</span>
+                                        </p>
+                                        <p>Status:
+                                          <span className="text-warning"
+                                                style={{"fontWeight": "bold"}}>{item.part.data.status}</span>
+                                        </p>
+                                      </Col>
+                                      <Col>
+                                        <Link to={item.link}>
+                                          <Button className="button-product-list">Details</Button>
+                                        </Link>
+                                        <Button className="button-product-list"
+                                                onClick={() => this.handleAddToFav(item)}><FaStar
+                                          size={20}/></Button>
+                                      </Col>
+                                    </Row>
+                                  </Grid>
+                                </Panel>
+                              </div>
+                            )
+                          })
+                        :
+                        <li>Loading...</li>
+                    }
+                  </ListGroup>
+                </Tab>
+                <Tab eventKey={2} title="Grid">
+                  <Grid>
+                    <Row>
+                      {
+                        this.state.parts.length ?
+                          this.state.parts.map(
+                            (item, m) => {
+                              return (
+                                <Col key={m} xs={6} md={4}>
+                                  <Thumbnail src={item.part.jpg[0]} alt="Picture of part">
+                                    <h3>{item.part.data.name}</h3>
+                                    <p>Brand: <span className="text-info">{item.part.data.brand}</span>
+                                    </p>
+                                    <p>Number: <span className="text-info">{item.part.data.number}</span>
+                                    </p>
+                                    <p>Status:
+                                      <span className="text-warning"
+                                            style={{"fontWeight": "bold"}}>{item.part.data.status}</span>
+                                    </p>
+                                    <p>
+                                      <Link to={item.link}>
+                                        <Button className="button-product-list">Details</Button>
+                                      </Link>
+                                      <Button className="button-product-list"
+                                              onClick={() => this.handleAddToFav(item)}><FaStar
+                                        size={20}/></Button>
+                                    </p>
+                                  </Thumbnail>
                                 </Col>
-                                <Col xs={6} md={8}>
-                                  <p>Brand: <span className="text-info">{item.part.data.brand}</span>
-                                  </p>
-                                  <p>Number: <span className="text-info">{item.part.data.number}</span>
-                                  </p>
-                                  <p>Status:
-                                    <span className="text-warning"
-                                          style={{"fontWeight": "bold"}}>
-                                    {item.part.data.status}
-                                    </span>
-                                  </p>
-                                </Col>
-                                <Col>
-                                  <Link to={item.link}>
-                                    <Button>Details</Button>
-                                  </Link>
-                                  <Button
-                                    onClick={() => this.handleAddToFav(item)}><FaStar
-                                    size={20}/></Button>
-                                </Col>
-                              </Row>
-                            </Grid>
-                          </Panel>
-                        </div>
-                      )
-                    })
-                  :
-                  <li>Loading...</li>
-              }
-            </ListGroup>
+                              )
+                            })
+                          :
+                          <li>Loading...</li>
+                      }
+                    </Row>
+                  </Grid>
+                </Tab>
+              </Tabs>
+            </div>
             <br/>
 
           </Col>
