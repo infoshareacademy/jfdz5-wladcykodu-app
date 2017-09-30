3cd4175663af23ae68608bb43bebe66c9590713c
rafals7 <rafal.sobiepan@gmail.com>
2017-09-30 17:42:42 +0200

    feature/FZ5WK-64-RS: Now cascade of dropdown lists are based on data in global store

--- src/components/views/PartsView.js
+++ src/components/views/PartsView.js
@@ -29,7 +29,7 @@ class PartsView extends Component {
     ).then(
       result => {
         //this.props.setTree(level, {data: result.data, datatype: result.datatype}, 0)
-        this.props.setTree(level, result, 0)
+        this.props.setTree(level, result, position)
         // this.result = {
         //   data: result.data,
         //   datatype: result.datatype
@@ -41,11 +41,46 @@ class PartsView extends Component {
   }
 
   handleSelect = event => {
-    //console.log(this.inputSelect.selectedIndex)
-    let inputLevel = event.currentTarget.getAttribute('data-level')
-    let inputPosition = event.currentTarget.selectedIndex
-    console.log('inputPosition:', inputPosition)
-    console.log('inputLevel: ', inputLevel)
+    let inputLevel = parseInt(event.currentTarget.getAttribute('data-level'))
+    let newLevel = inputLevel + 1
+    let inputPosition = event.currentTarget.selectedIndex - 1
+
+    if (event.currentTarget.selectedIndex > 0) {
+      console.log('inputPosition:', inputPosition)
+      console.log('inputLevel: ', inputLevel)
+      console.log(this.props.dataNodes[inputLevel].datatype[inputPosition])
+      let url = this.props.dataNodes[inputLevel].data[inputPosition].link
+      console.log(`${API_URL}${url}`, newLevel, inputPosition)
+      this.fetchData(`${API_URL}${url}`, newLevel, inputPosition)
+    } else {
+      // tree/TRUNCATE, newLevel
+    }
+
+
+  }
+
+  getLabel = datatype => {
+    if (datatype) {
+      let label = '';
+      switch(datatype) {
+        case 'brands' :
+          label = 'Choose car brand:'
+          break;
+        case 'models':
+          label = 'Choose car model:'
+          break;
+        case 'types':
+          label = 'Choose engine type:'
+          break;
+        case 'tree':
+          label = 'Choose category:'
+          break;
+        default:
+          label = 'Choose option:'
+      }
+      return label
+    }
+
   }
 
   dataLevel = 1
@@ -63,6 +98,7 @@ class PartsView extends Component {
     // console.log('data1: ',data1)
     // // console.log('data1: ',data1, data1[0].data[0].name)
     // //console.log('data11: ',data11)
+    //console.log(this.props.dataNodes[0].data[0])
     // const data2 = [{datatype: "brands", data:[{id:"A01", name:"jeden"},{id:"A02", name:"dwa"},{id:"A03", name:"trzy"}]}]
     // console.log('data2: ', data2, data2[0].data[0].name)
     // //console.log(JSON.stringify(dataNodes))
@@ -72,31 +108,37 @@ class PartsView extends Component {
     return (
       <div>
 
-        {tdataNodes.map(
+        {dataNodes.map(
           (item, itemIndex) => {
+            if (item.datatype != 'stock') {
             return (
-              <FormGroup controlId="formControlsSelect" key={itemIndex}>
-                <ControlLabel>Choose car makes</ControlLabel>
-                <FormControl
-                  componentClass="select"
-                  placeholder="select"
-                  onChange={this.handleSelect}
-                  data-level={itemIndex}
-                  data-type={item.datatype}
-                  defaultValue="..."
-                  // inputRef={ inputSelect => this.inputSelect=inputSelect }
-                >
-                  <option value="..." key="0">...</option>
-                  {item.data.map((option, optionIndex) => {
-                      return (
-                        <option value={option.name} key={optionIndex + 1}>{option.name}</option>
-                      )
-                    }
-                  )}
-                </FormControl>
-              </FormGroup>
-
+                              <FormGroup controlId="formControlsSelect" key={itemIndex}>
+                  <ControlLabel>{this.getLabel(item.datatype)}</ControlLabel>
+                  <FormControl
+                    componentClass="select"
+                    placeholder="select"
+                    onChange={this.handleSelect}
+                    data-level={itemIndex}
+                    data-type={item.datatype}
+                    defaultValue="..."
+                    // defaultValue={item.data[2] ? item.data[2].name : item.data[0].name}
+                    // inputRef={ inputSelect => this.inputSelect=inputSelect }
+                  >
+                    <option value="..." key="0">...</option>
+                    {item.data.map((option, optionIndex) => {
+                        return (
+                          <option value={option.name} key={optionIndex + 1}>{option.name}</option>
+                        )
+                      }
+                    )}
+                  </FormControl>
+                </FormGroup>
             )
+              } else {
+               return (<h1>Tu będzie lista części</h1>)
+              }
+
+
 
           }
         )}
